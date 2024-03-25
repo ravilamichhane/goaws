6d676b0c5d4a2a3ad9baf03e5626efd5d5da4425
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-03-25 14:28:53 +0545

    hash password

--- lambda/go.mod
+++ lambda/go.mod
@@ -5,6 +5,7 @@ go 1.22.1
 require (
 	github.com/aws/aws-lambda-go v1.46.0
 	github.com/aws/aws-sdk-go v1.51.6
+	golang.org/x/crypto v0.21.0
 )
 
 require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- lambda/types/types.go
+++ lambda/types/types.go
@@ -1,6 +1,26 @@
 package types
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type RegisterUserRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type User struct {
+	Username     string
+	PasswordHash string
+}
+
+func NewUser(req RegisterUserRequest) (User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return User{}, err
+	}
+	return User{
+		Username:     req.Username,
+		PasswordHash: string(hashedPassword),
+	}, nil
 }
